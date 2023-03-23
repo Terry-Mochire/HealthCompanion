7ef7c7422d678532980c2154e6242e1dd1f923db
Terry Mochire <terrybmochire@gmail.com>
2023-03-23 18:25:21 +0300

    Add urls from services app.

--- HealthCompanion/settings.py
+++ HealthCompanion/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "country.apps.CountryConfig",
+    "location.apps.CountryConfig",
     "clinic.apps.ClinicConfig",
     "service.apps.ServiceConfig",
 ]
--- HealthCompanion/urls.py
+++ HealthCompanion/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("clinic.urls")),
+    path("", include("service.urls")),
 ]
