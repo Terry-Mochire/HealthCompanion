95a1973a5e843478d34f516d9c404d08c4b3ccee
Terry Mochire <terrybmochire@gmail.com>
2023-03-28 11:35:56 +0300

    Refactor to name the app location

--- HealthCompanion/settings.py
+++ HealthCompanion/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "location.apps.CountryConfig",
+    "location.apps.LocationConfig",
     "clinic.apps.ClinicConfig",
     "service.apps.ServiceConfig",
 ]
