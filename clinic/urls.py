d95266c6d1f864913ea2e99789c5c46ecb028811
Terry Mochire <terrybmochire@gmail.com>
2023-04-10 18:14:56 +0300

    Return json objects.

--- clinic/urls.py
+++ clinic/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", about_us, name="about_us"),
-    path("clinics", ClinicListView.as_view(), name="clinics"),
-    path("clinic_detail/<int:pk>", ClinicDetailView.as_view(), name="clinic_detail"),
-    path("search_by_name", search_by_name, name="search_by_name"),
+    path("get_csrf_token", get_csrf_token, name="get_csrf_token"),
+    path("clinics", clinics, name="clinics"),
+    path("clinic_detail/<int:pk>", clinic_detail, name="clinic_detail"),
+    path("search_by_name/<str:name>", search_by_name, name="search_by_name"),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,6 @@
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse
+from django.middleware.csrf import get_token
 from django.shortcuts import render
 from django.views import generic
 
@@ -6,29 +9,27 @@ from .models import Clinic
 
 # Create your views here.
 
-class ClinicListView(generic.ListView):
-    model = Clinic
-    template_name = 'clinic/clinic_list.html'
-    context_object_name = 'clinics'
-
-    def get_queryset(self):
-        return Clinic.objects.all()
+def clinics(request):
+    allClinics = Clinic.objects.all()
+    data = serialize('json', allClinics)
+    return HttpResponse(data, content_type="application/json")
 
 
-class ClinicDetailView(generic.DetailView):
-    model = Clinic
-    template_name = 'clinic/clinic_detail.html'
-    context_object_name = 'clinic'
+def clinic_detail(request, pk):
+    clinic = Clinic.objects.get(pk=pk)
+    data = serialize('json', [clinic], fields=('name', 'country', 'county', 'sub_county', 'telephone', 'email', 'doctor_in_charge', 'list_of_services'))
+    return HttpResponse(data, content_type="application/json")
 
 
-def about_us(request):
-    return render(request, 'about_us.html')
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return JsonResponse({'csrf_token': csrf_token})
 
 
-def search_by_name(request):
+def search_by_name(request, name):
     if request.method == 'POST':
-        search_text = request.POST['search_text']
+        search_text = name
     else:
         search_text = ''
-    clinics = Clinic.objects.filter(name__contains=search_text)
-    return render(request, 'clinic/clinic_list.html', {'clinics': clinics})
+    clinic_by_name = Clinic.objects.filter(name__contains=search_text)
+    return HttpResponse(serialize('json', clinic_by_name), content_type="application/json")
