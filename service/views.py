e5123b1215118c3cf02b6b4c6dc8bea9d73cbab0
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 13:36:05 +0300

    Refactor views to return JSON types

--- service/views.py
+++ service/views.py
@@ -1,4 +1,6 @@
 import generic as generic
+from django.core.serializers import serialize
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from clinic.models import Clinic
@@ -8,19 +10,23 @@ from django.views import generic
 
 # Create your views here.
 
-class ServiceListView(generic.ListView):
-    model = Service
-    template_name = "service/services.html"
-    context_object_name = "services"
 
-    def get_queryset(self):
-        return Service.objects.all()
+def get_services(request):
+    allServices = Service.objects.all()
+    data = serialize('json', allServices)
+    return HttpResponse(data, content_type="application/json")
 
 
-class ServiceDetailView(generic.DetailView):
-    model = Service
-    template_name = "service/service_detail.html"
-    context_object_name = "service"
+def get_service_by_id(request, pk):
+    service = Service.objects.get(pk=pk)
+    data = serialize('json', [service])
+    return HttpResponse(data, content_type="application/json")
+
+
+def get_services_in_clinic(request, pk):
+    service = Service.objects.filter(clinic__id=pk)
+    data = serialize('json', service)
+    return HttpResponse(data, content_type="application/json")
 
 
 def search_clinics_with_service(request, service_name):
