28f1ecd77942ac38ecca513dcf6216c2840e17ea
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 13:33:28 +0300

    Add urls to retrieve the different location types.

--- location/urls.py
+++ location/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('search/', search_clinics_in_location, name='search'),
+    path('countries', get_countries, name='get_countries'),
+    path('counties', get_counties, name='get_counties'),
+    path('sub_counties', get_sub_counties, name='get_sub_counties'),
+    path('sub_county/<int:county_id>', get_sub_counties_by_county, name='get_sub_counties_by_county'),
 ]
