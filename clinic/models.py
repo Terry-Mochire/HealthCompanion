d321f2abd1043e7a0f0a525121e4aa822c6a875e
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 13:32:09 +0300

    Add image and address fields.

--- clinic/models.py
+++ clinic/models.py
@@ -13,6 +13,8 @@ class Clinic(models.Model):
     sub_county = models.ForeignKey(SubCounty, on_delete=models.CASCADE)
     telephone = models.CharField(max_length=120, null=True, blank=True)
     email = models.CharField(max_length=120, null=True, blank=True)
+    image = models.CharField(max_length=500, null=True, blank=True)
+    address = models.CharField(max_length=250, null=True, blank=True)
     doctor_in_charge = models.CharField(max_length=120, null=True, blank=True)
     list_of_services = models.ManyToManyField(Service, blank=True)
 
