43e2e3898cb84318c63c6771e8a723229b7f1b63
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 13:35:43 +0300

    Add views for the different location types.

--- location/views.py
+++ location/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+import json
+
+from django.core.serializers import serialize
+from django.http import HttpResponse
 
 from clinic.models import Clinic
-from location.forms import LocationForm
 from location.models import Country, County, SubCounty
 
 # Create your views here.
@@ -12,17 +14,46 @@ counties = County.objects.all()
 sub_counties = SubCounty.objects.all()
 
 
+def get_countries(request):
+    allCountries = Country.objects.all()
+    data = serialize('json', allCountries)
+    return HttpResponse(data, content_type="application/json")
+
+
+def get_counties(request):
+    allCounties = County.objects.all()
+    data = serialize('json', allCounties)
+    return HttpResponse(data, content_type="application/json")
+
+
+def get_sub_counties(request):
+    allSubCounties = SubCounty.objects.all()
+    data = serialize('json', allSubCounties)
+    return HttpResponse(data, content_type="application/json")
+
+
+def get_sub_counties_by_county(request, county_id):
+    allSubCounties = SubCounty.objects.filter(county__id=county_id)
+    data = serialize('json', allSubCounties)
+    return HttpResponse(data, content_type="application/json")
+
+
 def search_clinics_in_location(request):
     if request.method == 'POST':
-        country_id = request.POST.get('country')
-        county_id = request.POST.get('county')
-        sub_county_id = request.POST.get('sub_county')
+        data = request.body.decode('utf-8')
 
+        data_dict = json.loads(data)
+        country_id = data_dict.get('country')
+        county_id = data_dict.get('county')
+        sub_county_id = data_dict.get('sub_county')
         print(country_id, county_id, sub_county_id)
-
-        clinics = Clinic.objects.filter(country=country_id, county=county_id, sub_county=sub_county_id)
-        return render(request, 'clinic/clinic_list.html', {'clinics': clinics})
-    else:
-        form = LocationForm()
-    return render(request, 'location/search.html', {'form': form, 'countries': countries, 'counties': counties,
-                                                    'sub_counties': sub_counties})
+        if country_id:
+            clinics = Clinic.objects.filter(country__id=country_id)
+        if county_id:
+            clinics = Clinic.objects.filter(country__id=country_id, county__id=county_id)
+        if sub_county_id:
+            clinics = Clinic.objects.filter(country__id=country_id, county__id=county_id, sub_county__id=sub_county_id)
+        else:
+            clinics = Clinic.objects.all()
+        data = serialize('json', clinics)
+        return HttpResponse(data, content_type="application/json")
