893a48667ea890ac21d279eafad1565f2c16d198
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 13:33:52 +0300

    Add new urls.

--- service/urls.py
+++ service/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("services", ServiceListView.as_view(), name="services"),
-    path("service_detail/<int:pk>", ServiceDetailView.as_view(), name="service_detail"),
-    path("search_clinics_with_service/<str:service_name>", search_clinics_with_service, name="search_clinics_with_service"),
+    path("services", get_services, name="services"),
+    path("service/<int:pk>", get_service_by_id, name="service_by_id"),
+    path("services_in_clinic/<int:pk>", get_services_in_clinic, name="services_in_clinic"),
+    path("search_clinics_with_service/<str:service_name>", search_clinics_with_service,
+         name="search_clinics_with_service"),
 ]
